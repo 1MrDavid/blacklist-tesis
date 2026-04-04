3c390dae67599be8bdea6f02fa2b49d59fbe2ea5
MrDavid <mrdavid1217@gmail.com>
2026-04-03 20:40:02 -0400

    Logica eliminacion de lista negra cuando lista blanca

--- backend-interfaces/src/main/java/com/demo/tesis/repository/BlacklistRepository.java
+++ backend-interfaces/src/main/java/com/demo/tesis/repository/BlacklistRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
     boolean existsByDocumento(String documento);
+    void deleteByDocumento(String documento);
     // boolean existsByIp(String ip); // Descomenta si pasas la IP desde el FrontEnd
 }
--- backend-interfaces/src/main/java/com/demo/tesis/service/WhitelistService.java
+++ backend-interfaces/src/main/java/com/demo/tesis/service/WhitelistService.java
@@ -1,9 +1,11 @@
 package com.demo.tesis.service;
 
 import com.demo.tesis.model.Whitelist;
+import com.demo.tesis.repository.BlacklistRepository;
 import com.demo.tesis.repository.WhitelistRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,12 +14,19 @@ import java.util.List;
 public class WhitelistService {
 
     private final WhitelistRepository whitelistRepository;
+    private final BlacklistRepository blacklistRepository;
 
     public List<Whitelist> getAllWhitelist() {
         return whitelistRepository.findAll();
     }
 
+    @Transactional
     public Whitelist saveWhitelist(Whitelist whitelist) {
+
+        if (blacklistRepository.existsByDocumento(whitelist.getDocumento())) {
+            blacklistRepository.deleteByDocumento(whitelist.getDocumento());
+        }
+
         return whitelistRepository.save(whitelist);
     }
 
